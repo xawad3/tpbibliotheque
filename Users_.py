8e8f4b26c280921c5affc1b6be54f6d498452ee4
Adeline <adeline.mdg@gmail.com>
2022-01-27 15:30:12 +0100

    créa class person & users

---
+++ Users_.py
@@ -0,0 +1,17 @@
+from Person_ import *
+
+class Users(Person):
+
+    def __init__(self, name, firstName, pwd):
+        super().__init__(name, firstName, pwd)
+        self.rank = 0
+        self.borrow = []
+
+    def Borrow:
+
+            pass
+
+
+    def __repr__(self):
+        affiche = f"{self.name} {self.firstName} {self.id} {self.rank}"
+        return affiche
