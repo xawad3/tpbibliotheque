2159f449ff0a45c010a4abe5c2f7f352367cb510
Zeshooter <romainsoulie1973@gmail.com>
2022-01-27 15:44:48 +0100

    V1.0

---
+++ Books_.py
@@ -0,0 +1,12 @@
+import random
+
+class Books:
+    def __init__(self, title, author, language, type, category):
+        self.title=title
+        self.author=author
+        self.language=language
+        self.type=type
+        self.category=category
+        self.ref=
+        self.dispo=False
+        self.backto=backto
---
+++ Comic_strip.py
@@ -0,0 +1,7 @@
+from Books_ import *
+
+class ComicStrip(Books) :
+    def __init__(self, title, author, language, type, category,color, designer):
+    super().__init__(self, title, author, language, type, category)
+
+
