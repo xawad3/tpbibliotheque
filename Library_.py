46ca7cc1670597769c61297e03757f8523864277
Newix17 <luniversderic@gmail.com>
2022-01-31 16:52:29 +0100

    library_v2 en sauvegardant cette foit :/

--- Library_.py
+++ Library_.py
@@ -12,29 +12,32 @@ class Library:
         self.books = []
         self.users = []
         
-  #  def __repr__(self):
-   #     choix = int(input(f"Ouvrages contenu dans la bibliothèque {self.name}:\nTapez "1" pour l'affichage par catégorie\nTapez "2" pour l'affichage par auteur"))
-   #     if choix == 1:
-   #         affiche = f"Voici la liste des livres, triés par catégorie: {self.section}"
-  #      if choix == 2:
-   #         affiche = f"Voici la liste des livres, triés par auteurs: {self.author}"
+    def __repr__(self):
+        choix = input(f"Ouvrages contenu dans la bibliothèque {self.name}:\nTapez "1" pour l'affichage par catégorie\nTapez "2" pour l'affichage par auteur\n")
+        if choix == 1:
+            affiche = f"Voici la liste des livres, triés par catégorie: {self.section}\n"
+        elif choix == 2:
+            affiche = f"Voici la liste des livres, triés par auteurs: {self.author}\n"
 
-    def getName():
+    def getName(self):
         return self.name
 
     def add_a_book(self, book):
+        self.books.append(self)
         with open("list_books.txt", 'a') as f:
             f.write(book)
 
     def remove_a_book(self, book):
+        self.books.remove(self)
         with open("list_books.txt", "r+") as f:
             f.truncate(book)
 
     def add_user(self):
-        self.users.append(new_user)
+        self.users.append(self)
         with open("list_users.txt", "a") as f:
-            f.write(new_user)
+            f.write(self)
 
     def remove_user(self):
+        self.users.remove(self)
         with open("list_users.txt", "r+") as f:
-            f.truncate()
+            f.truncate(self)
