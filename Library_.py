e22218442f7626f27f5ca1a574a8823d0d383867
Newix17 <luniversderic@gmail.com>
2022-01-31 11:13:48 +0100

    modify library

--- Library_.py
+++ Library_.py
@@ -1,13 +1,7 @@
 
-
-from concurrent.futures.process import BrokenProcessPool
-from configparser import SectionProxy
-from ssl import ALERT_DESCRIPTION_CLOSE_NOTIFY
-
-
 class Library:
 
-    def __init__(self, name)
+    def __init__(self, name):
         self.name = name
         self.section = []
         self.author = []
