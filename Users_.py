ca08c4ded854b9ab9d11d4c3b3719e2ec435d4b5
Adeline <adeline.mdg@gmail.com>
2022-01-31 11:51:35 +0100

    test

--- Users_.py
+++ Users_.py
@@ -1,18 +1,24 @@
 from Person_ import *
+from Books_ import *
+
+
 
 class Users(Person):
 
     def __init__(self, name, firstName, pwd):
         super().__init__(name, firstName, pwd)
         self.rank = 0
-        self.borrow = []
+        self.borrow = [book1]
 
     def Borrow(self, book):
+        self.book = Books(self.title)
         self.borrow.append(book)
 
     def BackTo(self, book):
         self.borrow.pop(book)
 
     def __repr__(self):
-        affiche = f"{self.name} {self.firstName} son identifiant est {self.id} son rang est de {self.rank}"
+        affiche = f"{self.name} {self.firstName} son identifiant est {self.id} son rang est de {self.rank} il a emprunté {self.borrow}"
         return affiche
+
+print(user)
