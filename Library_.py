6d0b873cffadd83f8d92da6e66527399222af1b4
Newix17 <luniversderic@gmail.com>
2022-02-07 09:17:01 +0100

    lib_updt2

--- Library_.py
+++ Library_.py
@@ -8,3 +8,29 @@ class Library:
         self.author = []
         self.books = []
         self.users = []
+
+    def __repr__(self):
+    choix = int(input(f"Ouvrages contenu dans la bibliothèque {self.name}:\nTapez "1" pour l'affichage par catégorie\nTapez "2" pour l'affichage par auteur"))
+    if choix == 1:
+        affiche = f"Voici la liste des livres, triés par catégorie: {self.section}"
+    if choix == 2:
+        affiche = f"Voici la liste des livres, triés par auteurs: {self.author}"
+
+    def getName():
+        return self.name
+
+    def add_a_book(book):
+        with open("list_books.txt", 'a') as f:
+            f.write(book)
+
+    def remove_a_book(book):
+        with open("list_books.txt", "r+") as f:
+            f.truncate(book)
+
+    def add_user(name):
+        with open("list_users.txt", "a") as f:
+            f.write(user)
+
+    def remove_user(name):
+        with open("list_users.txt", "r+") as f:
+            f.truncate(user)
