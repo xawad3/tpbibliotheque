9e508c4c51e2d4f506263fb0f7e7336355b79c24
Newix17 <luniversderic@gmail.com>
2022-02-08 15:49:06 +0100

    updt10

--- Library_.py
+++ Library_.py
@@ -25,31 +25,36 @@ class Library:
     def export_books(self):
         with open('list_books.txt', 'w') as f:
             for item in self.books:
-                f.write(str(item.title) +" ; "+ str(item.author) +" ; "+ str(item.language) +" ; "+ str(item.category) +" ; "+ str(item.ref)+"\n")
+                f.write(str(item.title) +" ; "+ str(item.author) +" ; "+ str(item.language) +" ; "+ str(item.type) +" ; "+ str(item.category) +" ; "+ str(item.ref)"\n")
 
     def import_books(self):
         with open("list_books.txt", 'r') as f:
             for item in f:
                 maLigne = item.split(" ; ")
-                print(maLigne)
-                self.books.append(Books(maLigne[0],maLigne[1],maLigne[2],maLigne[3],maLigne[4]))
+                self.books.append(Books(title=maLigne[0],author=maLigne[1],language=maLigne[2],type=maLigne[3],category=maLigne[4],ref=maLigne[5]))
                 if maLigne[1] not in self.author:
                     self.author.append(maLigne[1])
                 if maLigne[3] not in self.section:
                     self.section.append(maLigne[3])
-                # it works, reste à eviter les doublons
+                self.books[-1].ref = maLigne[5]
+            
                 
 
-    def add_user(self, user):
+    def search_author_list(self):
+        pass
+
+
+    def export_user(self, user):
         with open("list_users.txt", "a") as f:
-            maChaine = user.name + ";" + user.firstName + ";" + user.id + ";" + str(user.rank) + ";" + str(user.borrow)
-            f.write(maChaine)
+            maChaine = user.name + " ; " + user.firstName + " ; " + user.id + " ; " + str(user.rank) + " ; " + str(user.borrow)
+            if maChaine not in f:
+                f.write(maChaine)
 
-    def remove_user(self, user):
-        with open("list_users.txt", "r+") as f:
+    # def remove_user(self, user):
+    #     with open("list_users.txt", "r+") as f:
 
-            maChaine = user.name + ";" + user.firstName + ";" + user.id + "." + str(user.rank) + ";" + user.borrow
-            f.truncate(maChaine)
+    #         maChaine = user.name + ";" + user.firstName + ";" + user.id + "." + str(user.rank) + ";" + user.borrow
+    #         f.truncate(maChaine)
 
 biblio = Library("ABC")
 biblio.add_a_book(book1)
@@ -58,6 +63,7 @@ biblio.add_a_book(book3)
 biblio.add_a_book(book4)
 biblio.add_a_book(book5)
 biblio.export_books()
+print(biblio.books[0].ref)
 input("on vient d'exporter la liste en .txt\" maintenant efface la liste self.books et fait l'import")
 biblio.books = []
 input("normalement la liste self.books est vide")
@@ -68,3 +74,14 @@ input("maintenant je tente d'afficher juste les auteurs")
 print(biblio.author)
 input("et là les catégories")
 print(biblio.section)
+
+
+maLigne = "m.prybylo;maxime;przybylo;Azerty77;999999;[HJ1924893,AR9248837]"  
+mesEmprunts = []  
+#maLigne.split(";") permet de créer une liste en divisant ma chaine de caractère par ";" 
+#maLigne.split(";")[-1] permet de recupérer la derniere "case" en tant que str 
+#maLigne.split(";")[-1][1:-1] permet de ne garder que du deuxieme caractère (inclus) au dernier (non inclus) 
+#le dernier split permet de créer une liste en divisant mes ref par "," 
+for i in (maLigne.split(";")[-1])[1:-1].split(","):     
+  mesEmprunts.append(i)  
+  print(mesEmprunts)
