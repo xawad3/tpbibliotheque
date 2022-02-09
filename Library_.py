7b349b5d183908b04d7fc5645c5c946e87ebde86
Newix17 <luniversderic@gmail.com>
2022-02-09 10:22:38 +0100

    maj nom_livres

--- Library_.py
+++ Library_.py
@@ -19,20 +19,25 @@ class Library:
     def getName(self):
         return self.name_library
 
+# Pour Créer (ajouter) un livre dans books_list
+# =============================================
     def add_a_book(self, b):
         self.books_list.append(b)
         
+# Exportation des livres contenus dans books_list => list_books.txt
+# =================================================================
     def export_books(self):
         with open('list_books.txt', 'w') as f:
             for item in self.books_list:
                 f.write(str(item.title_book) +" ; "+ str(item.author_book) +" ; "+ str(item.language_book) +" ; "+ str(item.type_book) +" ; "+ str(item.category_book) +" ; "+ str(item.ref_book) + "\n")
 
+# Importation des livres du .txt => books_list
+# ============================================
     def import_books(self):
         with open("list_books.txt", 'r') as f:
             for item in f:
                 maLigne = item.split(" ; ")
                 self.books_list.append(Books(title_book=maLigne[0],author_book=maLigne[1],language_book=maLigne[2],type_book=maLigne[3],category_book=maLigne[4]))
-                #self.users_list[-1].rank = maLigne[3]
                 self.books_list[-1].ref_book = maLigne[5]
                 if maLigne[1] not in self.author_list:
                     self.author_list.append(maLigne[1])
@@ -40,16 +45,17 @@ class Library:
                     self.section_list.append(maLigne[4])
                 self.books_list[-1].ref = maLigne[5]
             
-                
-
     def search_author_list(self):
-        pass
+        for i in self.books_list:
+            print(i.author_book)
+
 
     def export_user(self, user):
         with open("list_users.txt", "a") as f:
             maChaine = user.name_user + " ; " + user.first_name_user + " ; " + user.id + " ; " + str(user.rank) + " ; " + str(user.borrow) + " ; " + str(user.counter_rank)
             if maChaine not in f:
                 f.write(maChaine)
+
 
     def import_user(self):
         with open("list_users.txt", 'r') as f:
@@ -62,28 +68,24 @@ class Library:
                     self.users_list[-1].borrow.append(i)
                 self.users_list[-1].counter = maLigne[4]
 
-
 biblio = Library("ABC")
 biblio.add_a_book(book1)
 biblio.add_a_book(book2)
-input("Je vais print books_list qui contient book1&2")
-print(biblio.books_list)
-input("Voilà, maintenant je vais l'exporter sur le .txt")
-biblio.export_books()
-input("jusque là tout va bien... j'efface la books_list")
-biblio.books_list = []
-print(biblio.books_list)
-input("Là j'importe depuis le .txt pour remplir book_list, author_list et section_list")
-biblio.import_books()
-input("la books_list:")
-print(biblio.books_list)
-input("la author_list:")
-print(biblio.author_list)
-input("la section_list:")
-print(biblio.section_list)
+biblio.search_author_list()
 
 
-
-
-  
-
+# input("Je vais print books_list qui contient book1&2")
+# print(biblio.books_list)
+# input("Voilà, maintenant je vais l'exporter sur le .txt")
+# biblio.export_books()
+# input("jusque là tout va bien... j'efface la books_list")
+# biblio.books_list = []
+# print(biblio.books_list)
+# input("Là j'importe depuis le .txt pour remplir book_list, author_list et section_list")
+# biblio.import_books()
+# input("la books_list:")
+# print(biblio.books_list)
+# input("la author_list:")
+# print(biblio.author_list)
+# input("la section_list:")
+# print(biblio.section_list)
